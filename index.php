d5418ad1ab47ef55670b03080181b17d6a6ecdca
philippoehrlein <dev@philippoehrlein.de>
2026-01-27 19:13:45 +0100

    chore(release): v1.1.2

--- index.php
+++ index.php
@@ -6,5 +6,5 @@ Kirby::plugin('philippoehrlein/typo-search-and-paste', [
   'translations' => require __DIR__ . '/config/translations.php',
   'api' => require __DIR__ . '/config/api.php',
   'options' => require __DIR__ . '/config/config.php',
-  'version' => '1.1.0'
+  'version' => '1.1.2'
 ]);
